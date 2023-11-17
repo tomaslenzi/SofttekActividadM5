5ea33e501201aaf427d73a93a4f9fce060f50176
tomaslenzi <tomaslenzi93@gmail.com>
2023-11-17 18:37:59 -0300

    eliminacion de metodo

--- src/main/java/com/example/softtekactividadm5/repository/TaskRepository.java
+++ src/main/java/com/example/softtekactividadm5/repository/TaskRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Integer> {
-    public Optional<Task> findById(int id);
+
 }
